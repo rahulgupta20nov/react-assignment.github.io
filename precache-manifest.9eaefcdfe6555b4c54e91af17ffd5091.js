6128afcac2f3fa959c795d820a218b59a548a0c6
Rahul Gupta <rahul.gupta@pearson.com>
2019-12-02 19:09:50 +0530

    first commit

---
+++ precache-manifest.9eaefcdfe6555b4c54e91af17ffd5091.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d861f4a3634be3b72b22ba95676d81c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f3e94a4e9cf2bac50c1",
+    "url": "/static/css/main.0f66c7bf.chunk.css"
+  },
+  {
+    "revision": "d3f2ad4b89053a31e983",
+    "url": "/static/js/2.e447da37.chunk.js"
+  },
+  {
+    "revision": "3f3e94a4e9cf2bac50c1",
+    "url": "/static/js/main.aa801895.chunk.js"
+  },
+  {
+    "revision": "06bc4db017172d458f20",
+    "url": "/static/js/runtime-main.a38b4661.js"
+  },
+  {
+    "revision": "7c858c1e9e6c971cc360141e92fc918e",
+    "url": "/static/media/Sample-jpg-image-500kb.7c858c1e.jpg"
+  }
+]);
